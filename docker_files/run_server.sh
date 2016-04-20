d3a3e5d5517df70bc6a4a7061e0fb6970a1c8a2c
root <root@condor2305.startdedicated.net>
2016-04-20 10:40:05 +0000

    Cleaned up run_server.sh, and don't supply level if it is 'Procedural Map', because that breaks things

--- docker_files/run_server.sh
+++ docker_files/run_server.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 cd /root/mounted
 export HOME="/root"
 file="./libGLU.so.1"
@@ -7,5 +7,26 @@ then
 	ln -s /usr/lib/x86_64-linux-gnu/mesa/libGL.so.1 "$file"
 fi
 
+RUST_ARGS="-batchmode"
+RUST_ARGS="$RUST_ARGS +server.hostname \"${RUST_SERVER_HOSTNAME}\""
+RUST_ARGS="$RUST_ARGS +server.ip ${RUST_SERVER_IP}"
+RUST_ARGS="$RUST_ARGS +server.port ${RUST_SERVER_PORT}"
+RUST_ARGS="$RUST_ARGS +server.identity ${RUST_SERVER_IDENTITY}"
+RUST_ARGS="$RUST_ARGS +server.seed ${RUST_SERVER_SEED}"
 
-exec ./RustDedicated -batchmode +server.hostname "${RUST_SERVER_HOSTNAME}" +server.ip ${RUST_SERVER_IP} +server.port ${RUST_SERVER_PORT} +server.identity ${RUST_SERVER_IDENTITY} +server.seed ${RUST_SERVER_SEED} +server.level "${RUST_SERVER_LEVEL}" +server.maxplayers ${RUST_SERVER_MAXPLAYERS} +server.worldsize ${RUST_SERVER_WORLDSIZE} +server.saveinterval ${RUST_SERVER_SAVEINTERVAL} +rcon.ip ${RUST_RCON_IP} +rcon.port ${RUST_RCON_PORT} +rcon.password "${RUST_RCON_PASSWORD}" +server.secure ${RUST_SERVER_SECURE} -logfile "${RUST_LOGFILE}"
+if [ "${RUST_SERVER_LEVEL}" != "Procedural Map" ]; then
+        RUST_ARGS="$RUST_ARGS +server.level \"${RUST_SERVER_LEVEL}\""
+fi
+
+RUST_ARGS="$RUST_ARGS +server.maxplayers ${RUST_SERVER_MAXPLAYERS}"
+RUST_ARGS="$RUST_ARGS +server.worldsize ${RUST_SERVER_WORLDSIZE}"
+RUST_ARGS="$RUST_ARGS +server.saveinterval ${RUST_SERVER_SAVEINTERVAL}"
+RUST_ARGS="$RUST_ARGS +rcon.ip ${RUST_RCON_IP}"
+RUST_ARGS="$RUST_ARGS +rcon.port ${RUST_RCON_PORT}"
+RUST_ARGS="$RUST_ARGS +rcon.password \"${RUST_RCON_PASSWORD}\""
+RUST_ARGS="$RUST_ARGS +server.secure ${RUST_SERVER_SECURE}"
+RUST_ARGS="$RUST_ARGS -logfile ${RUST_LOGFILE}"
+
+echo Arguments: $RUST_ARGS
+
+exec ./RustDedicated $RUST_ARGS
