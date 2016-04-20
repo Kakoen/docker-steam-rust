aa70f9c09eb4a696c00048b2ea0999f386b6b5b5
Koen de Kok <kakoen@gmail.com>
2016-04-20 10:51:43 +0200

    Added environment variables to configure server without having to change the docker image.

--- docker_files/run_server.sh
+++ docker_files/run_server.sh
@@ -8,4 +8,4 @@ then
 fi
 
 
-exec ./RustDedicated -batchmode +server.hostname "Experimental/Vanilla/NO WIPE" +server.port 27015 +server.identity "serverdatas" +server.seed 1177711122
+exec ./RustDedicated -batchmode +server.hostname ${RUST_SERVER_HOSTNAME} +server.ip ${RUST_SERVER_IP} +server.port ${RUST_SERVER_PORT} +server.identity ${RUST_SERVER_IDENTITY} +server.seed ${RUST_SERVER_SEED} +server.level ${RUST_SERVER_LEVEL} +server.maxplayers ${RUST_SERVER_MAXPLAYERS} +server.worldsize ${RUST_SERVER_WORLDSIZE} +server.saveinterval ${RUST_SERVER_SAVEINTERVAL} +rcon.ip ${RUST_RCON_IP} +rcon.port ${RUST_RCON_PORT} +rcon.password ${RUST_RCON_PASSWORD} +server.secure ${RUST_SERVER_SECURE} -logfile ${RUST_LOGFILE}
